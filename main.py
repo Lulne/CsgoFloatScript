7b72c8a97b8ec0a7ca34dbc326cc9e393549a60f
LulneWolfy <98662866+Lulne@users.noreply.github.com>
2024-05-01 20:44:17 -0500

    Update
    - added rate limit 
    - changed to getting API from secrets file

--- main.py
+++ main.py
@@ -4,17 +4,12 @@ import json
 from decimal import Decimal, getcontext, ROUND_HALF_UP
 from fuzzywuzzy import process
 import time
-
-
-# Fetches collection data from the CSFloat-compatible API.
 def fetch_collections():
     url = "https://bymykel.github.io/CSGO-API/api/en/collections.json"
     response = requests.get(url)
     collections = response.json()
     return collections
 
-
-# Determines the next higher rarity level for a given skin rarity.
 def next_higher_rarity(current_rarity):
     rarity_order = [
         "rarity_common_weapon",
@@ -29,8 +24,6 @@ def next_higher_rarity(current_rarity):
     except (ValueError, IndexError):
         return current_rarity
 
-
-# Finds skin details from the collection data, using fuzzy string matching to handle slight mismatches in names.
 def find_skin_details(name, collections):
     all_skins = [(skin['name'], skin, col['name']) for col in collections for skin in col['contains']]
     best_match = process.extractOne(name, [skin_name for skin_name, _, _ in all_skins], score_cutoff=60)
@@ -46,8 +39,6 @@ def find_skin_details(name, collections):
         print(f"No close match found for {name}.")
         return None
 
-
-# Finds possible higher-tier skins that can result from trading up a set of skins.
 def find_possible_outcomes(input_skins, collections):
     unique_outcomes = set()
     for skin_input in input_skins:
@@ -56,22 +47,17 @@ def find_possible_outcomes(input_skins, collections):
             current_rarity = skin_details['rarity']['id']
             higher_rarity = next_higher_rarity(current_rarity)
             relevant_collections = [col for col in collections if col['name'] == skin_details['collection_name']]
-
             for collection in relevant_collections:
                 for potential_outcome in collection['contains']:
                     if potential_outcome['rarity']['id'] == higher_rarity:
                         unique_outcomes.add((potential_outcome['name'], potential_outcome['rarity']['name']))
-
     return sorted(list(unique_outcomes), key=lambda x: x[0])
 
-
-# Reads skin data from a local file.
 def read_skins_from_file(file_path):
     with open(file_path, 'r') as file:
         return json.load(file)
 
 
-# Calculates the required float value ranges for the 10th skin needed to achieve a target float when trading up.
 def find_required_float_for_tenth_skin(input_skins, target_float, precision=8):
     num_chosen_skins = 9
     target_float_decimal = Decimal(target_float).quantize(Decimal('1.' + '0' * precision), rounding=ROUND_HALF_UP)
@@ -80,9 +66,12 @@ def find_required_float_for_tenth_skin(input_skins, target_float, precision=8):
     for combo in itertools.combinations(input_skins, num_chosen_skins):
         current_total_float = sum(Decimal(skin['actual_float']) for skin in combo)
         exact_required_float = (target_float_decimal * (num_chosen_skins + 1) - current_total_float) / Decimal(1)
+        exact_required_float = exact_required_float.quantize(Decimal('1.' + '0' * precision), rounding=ROUND_HALF_UP)
 
         min_valid_float = exact_required_float - Decimal('0.00001')
         max_valid_float = exact_required_float + Decimal('0.00001')
+        min_valid_float = min_valid_float.quantize(Decimal('1.' + '0' * precision), rounding=ROUND_HALF_UP)
+        max_valid_float = max_valid_float.quantize(Decimal('1.' + '0' * precision), rounding=ROUND_HALF_UP)
 
         if min_valid_float < Decimal('0'):
             min_valid_float = Decimal('0')
@@ -98,8 +87,6 @@ def find_required_float_for_tenth_skin(input_skins, target_float, precision=8):
 
     return results
 
-
-# Fetches market listings from CSFloat API, respecting API rate limits by pausing after every 20 requests.
 def fetch_skins_from_market(min_float, max_float, market_hash_name, api_key):
     url = 'https://csfloat.com/api/v1/listings'
     headers = {'Authorization': api_key}
@@ -113,13 +100,19 @@ def fetch_skins_from_market(min_float, max_float, market_hash_name, api_key):
     response = requests.get(url, headers=headers, params=params)
     if response.status_code == 200:
         return response.json()
+    elif response.status_code == 429:
+        print("Rate limit exceeded, waiting for 30 minutes...")
+        time.sleep(1800)
+        return fetch_skins_from_market(min_float, max_float, market_hash_name, api_key)
     else:
         print(f"Failed to fetch data: {response.status_code}, {response.text}")
         return None
 
+def main():
+    with open('secrets.json', 'r') as file:
+        secrets = json.load(file)
+    api_key = secrets['api_key']
 
-# The main function orchestrates the trade-up calculation and market fetch process.
-def main(api_key):
     collections = fetch_collections()
     input_skins = read_skins_from_file('input_skins.json')
     target_float = Decimal('0.069696969')
@@ -140,45 +133,27 @@ def main(api_key):
 
     print(f"Calculating trade-up combinations for {selected_skin_name}...")
     results = find_required_float_for_tenth_skin(input_skins, target_float)
-    total_floats_checked = 0
     found_listings = 0
     valid_combinations = []
-    request_count = 0
 
     for result in results:
         combo_description = ', '.join([f"{skin['name']} ({skin['actual_float']})" for skin in result['combination']])
         min_required_float = result['min_required_float']
         max_required_float = result['max_required_float']
 
-        current_float = min_required_float
-        increment = Decimal('0.00001')
-
-        while current_float <= max_required_float:
-            if request_count >= 20:
-                print("Pausing for 30 seconds to manage API rate limits...")
-                time.sleep(30)  # Pause the execution for 30 seconds
-                request_count = 0  # Reset the request count after the pause
-
-            total_floats_checked += 1
-            listings = fetch_skins_from_market(float(current_float), float(current_float), selected_skin_name, api_key)
-            request_count += 1
-
-            if listings and listings.get('listings', []):
-                found_listings += 1
-                valid_combinations.append((combo_description, listings['listings']))
-
-            current_float += increment
+        listings = fetch_skins_from_market(float(min_required_float), float(max_required_float), selected_skin_name, api_key)
+        if listings and listings.get('listings', []):
+            found_listings += 1
+            valid_combinations.append((combo_description, listings['listings']))
 
         print(f"Combination: {combo_description}")
         print(f"Valid float range for 10th skin: {min_required_float} to {max_required_float}")
 
-    print(f"Checked {total_floats_checked} floats and found {found_listings} listings on the market.")
+    print(f"Checked ranges for {len(results)} combinations and found {found_listings} with listings on the market.")
     for combo, listings in valid_combinations:
         print(f"Valid Recipe: {combo}")
         for listing in listings:
             print(f"Market Link: {listing['inspect_link']}")
 
-
 if __name__ == "__main__":
-    api_key = 'yehJbtkTKfH-TdwevpZTyRTyvtP4IwKM'
-    main(api_key)
+    main()
